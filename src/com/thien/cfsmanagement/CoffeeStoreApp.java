7a59e6ac3956883c43c95776cc0e08e6cebb08b7
Thienlm30 <lmthien.30@gmail.com>
2024-01-19 12:59:25 +0700

    second upload

--- src/com/thien/cfsmanagement/CoffeeStoreApp.java
+++ src/com/thien/cfsmanagement/CoffeeStoreApp.java
@@ -1,10 +1,10 @@
-package com.thien.cfsmanagement.gui.viewer;
+package com.thien.cfsmanagement;
 
 public class CoffeeStoreApp {
     
     public static void main(String[] args) {
         
-        
+
 
 
     }
--- src/com/thien/cfsmanagement/bussiness/model/Ingredients.java
+++ src/com/thien/cfsmanagement/bussiness/model/Ingredients.java
@@ -2,4 +2,6 @@ package com.thien.cfsmanagement.bussiness.model;
 
 public class Ingredients {
     
+    
+
 }

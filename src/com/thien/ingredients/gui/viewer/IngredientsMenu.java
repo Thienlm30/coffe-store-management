3de20bb69f91a0e6965d89d75342e24bdc0e2cd7
Thienlm30 <lmthien.30@gmail.com>
2024-01-28 09:19:42 +0700

    upload

--- src/com/thien/ingredients/gui/viewer/IngredientsMenu.java
+++ src/com/thien/ingredients/gui/viewer/IngredientsMenu.java
@@ -4,10 +4,40 @@ import com.thien.ingredients.gui.utilities.Menu;
 
 public class IngredientsMenu {
 
-    private Menu menu;
+    private static Menu menu;
     
-    public static void menuIngredients() {
+    public static void menuIngredients(String menutitle) {
+        menu = new Menu(menutitle);
+        menu.addOption("Add an ingredient");
+        menu.addOption("Update ingredient information");
+        menu.addOption("Delete ingredient");
+        menu.addOption("Show all ingredients");
+        menu.addOption("Other: Return to main menu");
+
         
+        int choice = 0;
+
+        do {
+            menu.printMenu();
+            menu.getChoice();
+             switch (choice) {
+                case 1:
+                    
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    
+                    break;             
+                default:
+                System.out.println("----------------Main Menu---------------");
+                    break;
+             }
+        } while (choice > 0 );
     }
 
 
